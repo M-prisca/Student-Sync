1b422d25df361cd3f3975b95bc38e29eb96419a2
Prisca Masereli <priscamasereli@gmail.com>
2025-08-24 22:25:26 +0200

    Feat: Add NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import logo from "../assets/logo.png";
 
 const NavBar = () => {
-  return <></>;
+  return (
+    <>
+      <nav className="flex justify-between items-center h-16 mx-8">
+        <div className="flex  ">
+          <img className="h-6 " src={logo} alt="Logo" />
+          <p className="ml-2 text-green-900 font-bold">STUDENTSYNC</p>
+        </div>
+        <div className="flex gap-8 text-lg font-semibold">
+          <button className="text-green-500 rounded-lg hover:border-4 px-4 ">
+            Home
+          </button>
+          <button className="hover:border-4 px-2 rounded-lg">
+            Add Student
+          </button>
+          <button className="text-green-500 hover:border-4 px-2 rounded-lg">
+            All Students
+          </button>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default NavBar;
